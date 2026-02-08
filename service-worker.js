d6a5fc751ef8cd26a48af45a113a023860c2282c
Naaji <aliamjadian.3330@gmail.com>
2026-02-08 22:22:25 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
+  console.log('Service Worker Installed');
   event.waitUntil(
-    caches.open('my-cache').then((cache) => {
+    caches.open('static-v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
+        './',
+        './index.html'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
